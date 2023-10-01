f13455c04efb117d916b585e508855a68ded42f5
Takumi Abe <ta93abe@ta93abe.com>
2023-10-01 14:40:26 +0900

    home link

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,8 +1,9 @@
 import { Fuggles } from "next/font/google";
 import { styles } from "./Title.css";
+import Link from "next/link";
 
 const fuggles = Fuggles({weight: "400", subsets: ["latin"]});
 
 export function Title(){
-  return <div className={`${fuggles.className} ${styles.title}`}>tech blog by ta93abe</div>
+  return <Link href="/" className={`${fuggles.className} ${styles.title}`}>tech blog by ta93abe</Link>
 }
